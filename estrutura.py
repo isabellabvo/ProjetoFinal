8b601230673ff038ac1896a18d9ce591e125641f
Lila-Hadba <63017863+Lila-Hadba@users.noreply.github.com>
2020-05-18 11:39:07 -0300

    Update estrutura.py

--- estrutura.py
+++ estrutura.py
@@ -7,3 +7,5 @@ imagemdobloco = pygame.image.load('C:\\Users\\55119\\Desktop\\insper\\dessoft\\S
 bloco = pygame.transform.scale(imagemdobloco, (largura, altura))
 imagemfundo = pygame.image.load('assets/img/starfield.png').convert()
 fundo = pygame.transform.scale(imagemfundo, (WIDTH, HEIGHT))
+
+
