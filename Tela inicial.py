df8cb0c82dd774dbcd931ee874d7938d8dd645ab
Lila-Hadba <63017863+Lila-Hadba@users.noreply.github.com>
2020-06-10 17:51:51 -0300

    Update Tela inicial.py

--- Tela inicial.py
+++ Tela inicial.py
@@ -1,6 +1,17 @@
-#Tela inicial
+#Tela Inicial
 
-WIDTH = 1280
-HEIGHT = 800
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Isabella Oliveira e Lila Hadba')
+pygame.init() 
+
+mainClock = pygame.time.Clock()
+pygame.display.set_caption('Tower Blocks')
+screen = pygame.display.set_mode((480, 600))
+font = pygame.font.SysFont(None, 50)
+fundoinicial = pygame.image.load("construcao.jpg").convert()
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+ 
+click = False
