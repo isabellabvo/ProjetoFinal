8dc049a9ea6530345ad0c703d99a4626b8a7387d
isabellabvo <63018304+isabellabvo@users.noreply.github.com>
2020-06-12 22:04:44 -0300

    Update Imagens.py

--- Imagens.py
+++ Imagens.py
@@ -1,7 +1,16 @@
-#Fundo
-largura = 100
-altura = 50
-imagemdobloco = pygame.image.load('C:\\Users\\55119\\Desktop\\insper\\dessoft\\Square_34544.png').convert()
-bloco = pygame.transform.scale(imagemdobloco, (largura, altura))
-imagemfundo = pygame.image.load('assets/img/starfield.png').convert()
-fundo = pygame.transform.scale(imagemfundo, (WIDTH, HEIGHT))
+#Fundo padrão
+fundoinicial = pygame.image.load("construcao.jpg").convert()
+#Bloco Mario
+blocomario = pygame.image.load("bloco mario.png").convert()
+blocomario = pygame.transform.scale(blocomario, (100, 100))
+#Bloco Minecraft
+blocominecraft = pygame.image.load("bloco minecraft.jpg").convert()
+blocominecraft = pygame.transform.scale(blocominecraft, (100, 100))
+#Bloco Padrão
+bloconormal = pygame.image.load("block.png").convert()
+bloconormal = pygame.transform.scale(bloconormal, (130, 80))
+
+block_img = pygame.image.load('block.png').convert_alpha()
+block_img = pygame.transform.scale(block_img, (BLOCK_WIDTH, BLOCK_HEIGHT))
+
+#checar o bloco padrão e qual a gente vai usar
